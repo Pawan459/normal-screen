603d2db78cda95443ee079a80f82eee7f1d98423
Pawan <59849311+Pawan459@users.noreply.github.com>
2020-09-12 20:24:13 +0530

    Connect Screen Initiated

--- src/Components/Connect/ConnectComponent.jsx
+++ src/Components/Connect/ConnectComponent.jsx
@@ -1,18 +1,63 @@
 import React from 'react';
+// Libraries import here
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 //  All Components here
 import Header from '../Common/Header/Header';
 
 // Components SCSS here
 import './ConnectComponent.scss';
+import UserDetailComponent from './components/userDetailComponent/UserDetailComponent';
 
+// hardcoded data
+const data = [
+  {
+    name: "Luna Lovegood",
+    isActive: true,
+    role: "External Surveyor",
+    ID: 9876543
+  },
+  {
+    name: "Moaning Myrtle",
+    isActive: false,
+    role: "Internal Surveyor",
+    ID: 9876543
+  },
+  {
+    name: "Neville Longbottom",
+    isActive: true,
+    role: "External Surveyor",
+    ID: 9876543
+  }
+];
 
 export default function ConnectComponent() {
+
+  const renderUsersAvailable = currData => {
+    if (!currData) return null;
+    return currData.map((user, index) => <UserDetailComponent key= {index} user={user}/>)
+    return null;
+  }
+
   return (
     <div className="connect-component">
       <Header>
         <h3>Connect</h3>
       </Header>
+      <div className="search">
+        <input type="text" placeholder = "Type a name, Phone Number or Email address"/>
+        <FontAwesomeIcon className = "search-icon" icon = {faSearch}/>
+      </div>
+
+      <div className="dropdown-menu">
+        <select required>
+            <option value="" disabled selected hidden>Search By Role</option>
+            <option value="0">Role 1</option>
+            <option value="1">Role 2</option>
+        </select>
+      </div>
+      {renderUsersAvailable(data)}
     </div>
   );
 }
---
+++ src/Components/Connect/components/userDetailComponent/UserDetailComponent.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { X_STREAM, VIDEO_CALL, END_CALL } from "../../../../Assets";
+
+
+
+export default function UserDetailComponent({user}) {
+  return (
+    <div className = "user-detail-component">
+      <div
+        className={`online-status ${user.isActive ? "online" : "offline"}`}
+      />
+      <div className="user-details">
+        <div className="user-name">
+          <h4>{user.name}</h4>
+        </div>
+        <div className="user-role">{user.role}</div>
+        <div className="user-id">ID: {user.ID}</div>
+      </div>
+      <div className="grow" />
+      <div className="logo">
+        <img src={X_STREAM} alt="STREAM_LOGO" />
+        <img src={VIDEO_CALL} alt="VIDEO_CALL_LOGO" />
+        <img src={END_CALL} alt="CALL_LOGO" />
+      </div>
+    </div>
+  );
+}
